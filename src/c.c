2c0a9abeecae6578f596677809fe7b12f5d60568
Isocoram <lee.klese@icloud.com>
2026-07-21 19:08:49 +0200

    Small bug fixes including type fixing.

--- src/c.c
+++ src/c.c
@@ -14,11 +14,12 @@ typedef struct block {
     u32 count;
 } block_t;
 
-off_t get_file_size_in_bytes(const u8 *path) {
+off_t get_file_size_in_bytes(const i8 *path) {
     i32 file_descriptor = open(path, O_RDONLY);
     if (file_descriptor == -1) { perror("open failed"); return -1; }
     off_t file_length = lseek(file_descriptor, 0, SEEK_END);
     i32 file_close_status = close(file_descriptor);
+    (void)file_close_status;
     return file_length;
 }
 
@@ -31,6 +32,7 @@ block_t *create_block(void) {
     block->next = NULL;
     block->previous = NULL;
     block->count = 0;
+    return block;
 }
 
 block_t *chunk_memory(u32 byte_count) {
@@ -62,13 +64,13 @@ i32 get_block_count(block_t *list) {
     return count;
 }
 
-block_t *linked_list_source_file(const u8 *path) {
+block_t *linked_list_source_file(const i8 *path) {
     i32 file_descriptor = open(path, O_RDONLY);
     i32 file_size = get_file_size_in_bytes(path);
     block_t *block_linked_list = chunk_memory(file_size);
     block_t *tail = block_linked_list;
     i32 block_count = get_block_count(block_linked_list);
-    for (u32 i = 0; i < block_count; i++) {
+    for (u32 i = 0; i < (u32)block_count; i++) {
         lseek(file_descriptor, (BUFFER_SIZE - 1)*i, SEEK_SET);
         read(file_descriptor, tail->buffer, BUFFER_SIZE - 1);
         tail = tail->next;
@@ -93,16 +95,18 @@ void dump_data(block_t *list) {
 void free_list(block_t *list) {
     block_t *temporary_pointer = list;
     if (!(list->next)) { return; }
-    free(list);
     free_list(temporary_pointer->next);
+    free(list);
 }
 
 i32 main(i32 argc, i8 **argv) {
+    (void)argc;
     u32 filesize = get_file_size_in_bytes(argv[1]);
     block_t *linked_list = chunk_memory(filesize);
     block_t *writtenlist = linked_list_source_file(argv[1]);
     block_t *zerothblock = block_by_index(writtenlist, 0);
-    printf("\n%s\n\n", zerothblock->buffer);
+    (void)zerothblock;
+    //printf("\n%s\n\n", zerothblock->buffer);
     dump_data(writtenlist);
     free_list(linked_list);
     free_list(writtenlist);
